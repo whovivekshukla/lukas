f4e7b9241702226e740d07c6cb4b7542f3fe2b79
Vivek Shukla <vivekshuklahere@gmail.com>
2023-12-12 11:49:11 +0530

    update

--- app/page.tsx
+++ app/page.tsx
@@ -6,14 +6,14 @@ export default function Home() {
     <div className="flex flex-col items-center justify-center h-screen p-4">
       <div>
         <h1 className="text-4xl font-bold text-center">Fly</h1>
-        <p className="py-2">Your own Drone Inspection SaaS powered by AI</p>
+        <p className="py-2">
+          Your own Drone Inspection SaaS powered by Artificial Intelligence
+        </p>
       </div>
 
       <div className="p-">
         <Button>
-          <Link href={"/sign-in"}>
-            Get Started
-          </Link>
+          <Link href={"/sign-in"}>Get Started</Link>
         </Button>
       </div>
     </div>
