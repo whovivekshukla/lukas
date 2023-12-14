1b39a2b301b054c34e45f4bf8e5d7f739f2afa11
Vivek Shukla <vivekshuklahere@gmail.com>
2023-12-14 12:07:56 +0530

    fix

--- app/(dashboard)/missions/page.tsx
+++ app/(dashboard)/missions/page.tsx
@@ -1,27 +1,12 @@
 'use client'
-import { useEffect, useState } from "react";
+
 import { getAllMissions } from "@/lib/api";
 import Link from "next/link";
 import MissionCard from "@/components/MissionCardComponent";
 
-const Missions = () => {
-  const [missions, setMissions] = useState(null);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const res = await getAllMissions();
-        Array.isArray(res)
-          ? setMissions(res)
-          : console.error("Invalid response format. Expected an array:", res);
-      } catch (error) {
-        console.error("Error fetching missions:", error);
-        // Handle error (set state, show an error message, etc.)
-      }
-    };
-
-    fetchData();
-  }, []);
+const Missions = async () => {
+  const res = await getAllMissions();
+  // console.log(res.mission);
 
   return (
     <div className="container mx-auto mt-8">
@@ -30,16 +15,6 @@ const Missions = () => {
         <Link href="/missions/create">
           <button className="btn btn-primary">Create Mission</button>
         </Link>
-      </div>
-
-      <div>
-        {missions ? (
-          missions.map((mission, index) => (
-            <MissionCard key={index} mission={mission} />
-          ))
-        ) : (
-          <p className="text-gray-600">Loading missions...</p>
-        )}
       </div>
     </div>
   );
