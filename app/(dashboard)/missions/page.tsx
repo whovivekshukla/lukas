528127ed00db3272faf67dc24d39e4f14e5f45b1
Vivek Shukla <vivekshuklahere@gmail.com>
2023-12-14 15:45:04 +0530

    fixed waypoint not rendering problem

--- app/(dashboard)/missions/page.tsx
+++ app/(dashboard)/missions/page.tsx
@@ -4,23 +4,7 @@ import { useEffect, useState } from "react";
 import { getAllMissions } from "@/lib/api";
 import Link from "next/link";
 
-const MissionCard = ({ mission }) => {
-  return (
-    <div className="bg-white p-4 mb-4 rounded-md shadow-md">
-      {" "}
-      <p className="text-xl font-bold mb-2">{mission.name}</p>{" "}
-      <p className="text-gray-600">{mission.status}</p>{" "}
-      <p className="text-gray-600">{JSON.stringify(mission.waypoints)}</p>
-      <p className="text-gray-600">{mission.altitude}</p>{" "}
-      <p className="text-gray-600">{mission.speed}</p>{" "}
-      <div className="mt-4 flex items-center justify-between">
-        {" "}
-        <span className="text-sm text-gray-500">{mission.createdAt}</span>{" "}
-        <button className="text-blue-500 hover:underline">Delete</button>{" "}
-      </div>{" "}
-    </div>
-  );
-};
+import MissionCard from "@/components/MissionCardComponent";
 
 const Missions = () => {
   const [missions, setMissions] = useState(null);
--- components/MissionCardComponent.tsx
+++ components/MissionCardComponent.tsx
@@ -1,17 +1,17 @@
 const MissionCard = ({ mission }) => {
   return (
-    <div className="bg-white p-4 mb-4 rounded-md shadow-md">
-      {" "}
-      <p className="text-xl font-bold mb-2">{mission.name}</p>{" "}
-      <p className="text-gray-600">{mission.status}</p>{" "}
-      <p className="text-gray-600">{mission.waypoints}</p>{" "}
-      <p className="text-gray-600">{mission.altitude}</p>{" "}
-      <p className="text-gray-600">{mission.speed}</p>{" "}
+    <div className="bg-white p-4 mb-4 rounded-md shadow-md ">
+      <p className="text-xl font-bold mb-2">{mission.name}</p>
+      <p className="text-gray-600">{mission.status}</p>
+      <div className="max-h-32 overflow-y-auto">
+        <p className="text-gray-600">{JSON.stringify(mission.waypoints)}</p>
+      </div>
+      <p className="text-gray-600">{mission.altitude}</p>
+      <p className="text-gray-600">{mission.speed}</p>
       <div className="mt-4 flex items-center justify-between">
-        {" "}
-        <span className="text-sm text-gray-500">{mission.createdAt}</span>{" "}
-        <button className="text-blue-500 hover:underline">Delete</button>{" "}
-      </div>{" "}
+        <span className="text-sm text-gray-500">{mission.createdAt}</span>
+        <button className="text-blue-500 hover:underline">Delete</button>
+      </div>
     </div>
   );
 };
