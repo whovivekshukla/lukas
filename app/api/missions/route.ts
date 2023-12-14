7d6893b24a93b8cfa0978302c8fe3bd1018220e7
Vivek Shukla <vivekshuklahere@gmail.com>
2023-12-14 15:21:48 +0530

    fix

--- app/api/missions/route.ts
+++ app/api/missions/route.ts
@@ -3,17 +3,12 @@ import { prisma } from "@/lib/db";
 import { NextResponse } from "next/server";
 
 export const GET = async () => {
-  try {
-    const user = await getUserFromClerkId();
-    const mission = await prisma.mission.findMany({
-      where: {
-        userId: user.id,
-      },
-    });
+  const user = await getUserFromClerkId();
+  const missions = await prisma.mission.findMany({
+    where: {
+      userId: user.id,
+    },
+  });
 
-    return NextResponse.json(mission);
-  } catch (error) {
-    console.error("Error Finding mission:", error);
-    return NextResponse.json({ msg: "Something went wrong" });
-  }
+  return NextResponse.json({ missions });
 };
