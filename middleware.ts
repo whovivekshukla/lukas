8d967b5f1b27862a81bb83fa69bd359316555415
Vivek Shukla <vivekshuklahere@gmail.com>
2024-01-11 13:34:45 +0530

    changes

--- middleware.ts
+++ middleware.ts
@@ -2,16 +2,6 @@ import { authMiddleware } from "@clerk/nextjs";
 export default authMiddleware({
   publicRoutes: [
     "/",
-    "/api/inspection/:id",
-    "/api/test/:id",
-    "/api/flytbase/ros/flytos/navigation/access_request",
-    "/api/flytbase/ros/flytos/navigation/arm",
-    "/api/flytbase/ros/flytos/navigation/disarm",
-    "/api/flytbase/ros/flytos/navigation/take_off",
-    "/api/flytbase/ros/flytos/navigation/land",
-    "/api/flytbase/ros/flytos/navigation/waypoint_set",
-    "/api/flytbase/ros/flytos/navigation/waypoint_execute",
-    "/api/flytbase/ros/flytos/navigation/rtl",
   ],
   apiRoutes: ["/api(.*)"],
 });
