331410556dfab01d2cadf3dfc5efceddcd326e03
Vivek Shukla <vivekshuklahere@gmail.com>
2024-01-27 15:54:13 +0530

    fix

---
+++ app/temp-account/page.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import { useState } from "react";
+import { useSignIn } from "@clerk/nextjs";
+import { useRouter } from "next/navigation";
+
+export default function SignInForm() {
+  const { isLoaded, signIn, setActive } = useSignIn();
+  const router = useRouter();
+
+  const emailAddress = process.env.LUKAS_TEMP_ACCOUNT_EMAIL;
+  const password = process.env.LUKAS_TEMP_ACCOUNT_PASSWORD;
+
+  // start the sign In process.
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!isLoaded) {
+      return;
+    }
+
+    try {
+      const result = await signIn.create({
+        identifier: emailAddress,
+        password,
+      });
+
+      if (result.status === "complete") {
+        console.log(result);
+        await setActive({ session: result.createdSessionId });
+        router.push("/");
+      } else {
+        /*Investigate why the login hasn't completed */
+        console.log(result);
+      }
+    } catch (err: any) {
+      console.error("error", err.errors[0].longMessage);
+    }
+  };
+
+  return (
+    <div>
+      <form>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input
+            onChange={(e) => setEmailAddress(e.target.value)}
+            id="email"
+            name="email"
+            type="email"
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            onChange={(e) => setPassword(e.target.value)}
+            id="password"
+            name="password"
+            type="password"
+          />
+        </div>
+        <button onClick={handleSubmit}>Sign In</button>
+      </form>
+    </div>
+  );
+}
--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,12 @@
-import { authMiddleware } from "@clerk/nextjs";
+import { authMiddleware, redirectToSignIn } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 
 export default authMiddleware({
-  publicRoutes: ["/"],
-  apiRoutes: ["/api(.*)"],
+  publicRoutes: ["/", "/temp-account", "/sign-in"],
   afterAuth(auth, req, evt) {
+    if (!auth.userId && !auth.isPublicRoute) {
+      return redirectToSignIn({ returnBackUrl: req.url });
+    }
     if (auth.userId === "user_2bWyl83Oam3PoCgsrUAMsFFZOZI") {
       if (
         req.method === "POST" ||
