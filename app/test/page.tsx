b45a64791e68a7f2bf67a727f5bad5fd50518fd8
Vivek Shukla <vivekshuklahere@gmail.com>
2024-02-06 15:40:56 +0530

    changes

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,10 +1,4 @@
-import { analyzeVideo } from "@/lib/VideoIntelligenceAPI/api";
-
 const TestPage = async () => {
-  const res = await analyzeVideo();
-  console.log(res);
-
   return <div>TestPage</div>;
 };
 export default TestPage;
- 
