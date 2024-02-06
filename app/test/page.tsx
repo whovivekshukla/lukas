b25bfbea246ce486d6128cc88f58567a360889b1
Vivek Shukla <vivekshuklahere@gmail.com>
2024-02-06 16:59:07 +0530

    added code for videointelligense

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,10 +1,24 @@
-import { analyzeVideo } from "@/lib/VideoIntelligenceAPI/api";
+import {
+  analyzeVideo,
+  saveVideoToStorage,
+} from "@/lib/VideoIntelligenceAPI/api";
 
 const TestPage = async () => {
-  const res = await analyzeVideo();
-  console.log(res);
+  // const videoEndpoint =
+  //   "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/WeAreGoingOnBullrun.mp4";
+  // const googleCloudStorageBucket = "lukas-demo-video";
+  // const destinationFileName = "test-video";
 
+  //  const gsVideoLink = await saveVideoToStorage(
+  //   videoEndpoint,
+  //   googleCloudStorageBucket,
+  //   destinationFileName
+  // );
+
+  const gsLink = "gs://lukas-demo-video/test-video";
+
+   const videoAnalysis = await analyzeVideo(gsLink);
+  console.log(videoAnalysis)
   return <div>TestPage</div>;
 };
 export default TestPage;
- 
