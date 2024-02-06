d84db54d228c4132bab9dc55908707bb6e3faa86
Vivek Shukla <vivekshuklahere@gmail.com>
2024-02-06 14:56:39 +0530

    added loading dots

--- app/temp-account/page.tsx
+++ app/temp-account/page.tsx
@@ -35,5 +35,9 @@ export default function SignInForm() {
   };
   handleSubmit();
 
-  return <div>...loading</div>;
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <span className="loading loading-dots loading-lg"></span>
+    </div>
+  );
 }
