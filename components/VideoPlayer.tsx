9ff81564b3d0db9d51692f6428699319b256a3b9
Vivek Shukla <vivekshuklahere@gmail.com>
2024-02-09 23:02:09 +0530

    added video fix

--- components/VideoPlayer.tsx
+++ components/VideoPlayer.tsx
@@ -1,26 +1,23 @@
 import React from "react";
-import YouTube from "react-youtube";
+import ReactPlayer from "react-player";
 
 const Video = ({ videoUrl }) => {
   if (!videoUrl) {
     return <div>...loading</div>;
   }
-  // Extracting video ID from the URL
-  const videoId = videoUrl.split("v=")[1];
-
-  // Options for the YouTube player
-  const opts = {
-    height: "400",
-    width: "400",
-    playerVars: {
-      // https://developers.google.com/youtube/player_parameters
-      autoplay: 0,
-    },
-  };
 
   return (
     <div>
-      <YouTube videoId={videoId} opts={opts} />
+      <ReactPlayer
+        width="500px"
+        height="400px"
+        url={videoUrl}
+        controls={true}
+        // light is usefull incase of dark mode
+        light={false}
+        // picture in picture
+        pip={true}
+      />
     </div>
   );
 };
